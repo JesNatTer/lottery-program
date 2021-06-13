fefd2d36c1b84a0a5421a2dc0f56571fee9c1558
Jeandre Awood <jeandreross@gmail.com>
2021-06-13 16:01:25 +0200

    fixed error with function and import

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ root.config(bg='#e1ad01')
 root.title("Login")
 # date
 date = date.today()
+# variable
 
-def age_verfication(age):
+
+def age_verification():
     try: # error trapping
+        age = int(age_lbl.get())
         if age >= 18:
-           return messagebox.showinfo("Success", "Welcome To The Lotto Program")
-           root.destroy()
-           import lotto
+            messagebox.showinfo("Success", "Welcome To The Lotto Program")
+            root.destroy()
+            import lotto
+
         else:
            return messagebox.showinfo("Failure", "You need to be at least 18 to play the game")
     except ValueError: # error trapping
@@ -34,7 +38,7 @@ age_lbl = Entry(root, text="YYYY/MM/DD")
 age_lbl.place(x=95, y=30)
 
 # button
-confirm_btn = Button(root, borderwidth="10", text="Verify Age", font="Consolas 15 bold", fg="white", bg="black", command=age_verfication)
+confirm_btn = Button(root, borderwidth="10", text="Verify Age", font="Consolas 15 bold", fg="white", bg="black", command=age_verification)
 confirm_btn.place(x=85, y=55)
 
 # to run the program
